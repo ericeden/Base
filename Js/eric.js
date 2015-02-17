69b168d4f8f743a9078f74a4aa165455171efcd6
Eric Eden <ericedendev@gmail.com>
2015-02-17 16:34:58 +0300

    Added CSS JS & HTML Files

---
+++ Js/eric.js
@@ -0,0 +1,8 @@
+$(function() {
+
+$("h2")
+.prepend ('<img src="file:///C:/Users/Eric/Desktop/BASE/Images/left-frill.ico">')
+.append ('<img src="file:///C:/Users/Eric/Desktop/BASE/Images/right-frill.ico">');
+
+
+});
